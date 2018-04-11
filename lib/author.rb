8b0d063dbf08e4df77a133c7f16f171e5db601da
Cole Hart <cole@colehart.com>
2018-04-11 09:31:03 -0600

    added Author class and tests

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,12 @@
+# Author class
 class Author
+  attr_reader :first_name,
+              :last_name,
+              :books
 
+  def initialize(author_hash = Hash.new(0))
+    @first_name = author_hash[:first_name]
+    @last_name  = author_hash[:last_name]
+    @books      = []
+  end
 end
--- test/author_test.rb
+++ test/author_test.rb
@@ -3,10 +3,20 @@ require './lib/author'
 
 class AuthorTest < Minitest::Test
   def setup
-    @author = Author.new
+    @author = Author.new(first_name: 'Charlotte',
+                         last_name: 'Bronte')
   end
 
   def test_it_exists
     assert_instance_of Author, @author
+  end
+
+  def test_it_has_attributes
+    assert_equal 'Charlotte', @author.first_name
+    assert_equal 'Bronte', @author.last_name
+  end
+
+  def test_starts_with_no_books
+    assert_empty @author.books
   end
 end
